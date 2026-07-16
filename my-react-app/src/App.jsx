c4cfc35de0a717a7051472d2d422921c83ffaa71
KADIRIAmina <aminakadiri39@gmail.com>
2026-07-16 11:29:40 +0100

    feat: first Features

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,2 +1,32 @@
 import React from 'react';
-import {FaMoneyBill} from 'react-icons/fa';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "../pages/Home"
+import About from "../pages/About"
+import Vans from "../pages/Vans"
+import "./index.css"
+
+import "./server"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
