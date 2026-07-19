c1f1ccc8c037a691c2add0d44c7874681b1fa402
KADIRIAmina <aminakadiri39@gmail.com>
2026-07-19 10:30:51 +0100

    feat: adding features

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "../pages/Home"
 import About from "../pages/About"
 import Vans from "../pages/Vans"
-import "./index.css"
+import VanDetail from "../pages/VanDetails"
+import Dashboard from "../pages/Dashboard"
+import Income from "../pages/Income"
+import Reviews from "../pages/Reviews"
+import Layout from "../pages/Layout"
+import HostLayout from "../pages/HostLayout"
 
 import "./server"
 
 function App() {
+  /**
+   * Challenge: add the /host/vans and /host/vans/:id routes, as well
+   * as the "Vans" link in the Host navbar.
+   * 
+   * For now, just create the stubbed-out version of the pages (i.e.
+   * components that just render an <h1>). Don't worry about adding
+   * navigation from /host/vans to /host/vans/:id yet - the link to
+   * /host/vans is enough for now.
+   * 
+   * When deciding whether or not to use nested routes, keep in mind
+   * what will/won't be shared between these two pages. See the Figma
+   * design file (or the screenshots) to help guide your choice.
+   */
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
